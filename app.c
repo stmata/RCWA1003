e4b47bc858eccfdd6815704577321fad08d1e5c3
stmata <108939392+stmata@users.noreply.github.com>
2024-09-27 14:13:46 -0400

    Initial commit

---
+++ app.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+
+int i = 0, sum = 0;
+
+for(  ; i<= 10; )
+{
+    sum +=i;
+    i++;
+}
+printf("%d", sum);
---
+++ app.js
@@ -0,0 +1,3 @@
+setTimeout(() => {
+    console.log("The function will fire within a second!")
+}, 1000)
